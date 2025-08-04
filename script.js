ef9a97c62f7bbb03203bdfff54e917c104e9b602
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-04 12:47:30 +0200

    Work in progress

---
+++ script.js
@@ -0,0 +1,20 @@
+let button = document.getElementById("calculate");
+button.addEventListener("click", calculations);
+
+let field_X = document.getElementById("x");
+let field_X5000 = document.getElementById("x+5000");
+let field_X10000 = document.getElementById("x+10000");
+let field_X15000 = document.getElementById("x+15000");
+
+
+
+function calculations() {
+    let sum = document.getElementById("total_sum").value;
+    let incomeA = document.getElementById("income_A").value;
+    let incomeB = document.getElementById("income_B").value;
+}
+
+
+function calculate(field, sum, incomeA, incomeB) {
+
+}
