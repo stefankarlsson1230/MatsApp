a622b18f5aecd40ff07dd1e0bb6e1d55846f981c
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-05 10:43:06 +0200

    Finished basic funcionality and apperance.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let field_X5000 = document.getElementById("x+5000");
 let field_X10000 = document.getElementById("x+10000");
 let field_X15000 = document.getElementById("x+15000");
 
+let resultsDiv = document.getElementById("resultsDiv");
+
+resultsDiv.style.display = "none";
+
 field_X.innerText = "X";
 field_X5000.innerText = "X + 5000";
 field_X10000.innerText = "X + 10000";
@@ -20,12 +24,14 @@ function calculations() {
     calculate(field_X5000, sum + 5000, incomeA, incomeB);
     calculate(field_X10000, sum + 10000, incomeA, incomeB);
     calculate(field_X15000, sum + 15000, incomeA, incomeB);
+
+    resultsDiv.style.display = "block";
 }
 
 
 function calculate(field, sum, incomeA, incomeB) {
-    let amount_from_A = (sum * (incomeA / (incomeA + incomeB))).toFixed(2);
-    let amount_from_B = (sum - amount_from_A).toFixed(2);
+    let amount_from_A = Math.round(sum * (incomeA / (incomeA + incomeB)))
+    let amount_from_B = Math.round(sum - amount_from_A);
 
-    field.innerText = `${sum}\n\nFör ett gemensamt belopp på ${sum} kronor så ska A betala ${amount_from_A} kronor och B ska betala ${amount_from_B} kronor. A kommer få ${(incomeA - amount_from_A).toFixed(2)} kronor kvar och B kommer få ${(incomeB - amount_from_B).toFixed(2)} kronor kvar`;
+    field.innerText = `${sum} kr\n\nFör ett gemensamt belopp på ${sum} kronor så ska A betala ${amount_from_A} kronor och B ska betala ${amount_from_B} kronor. A kommer få ${incomeA - amount_from_A} kronor kvar och B kommer få ${incomeB - amount_from_B} kronor kvar`;
 }
